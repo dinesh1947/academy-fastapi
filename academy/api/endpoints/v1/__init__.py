4824f2ec90c80b74c7bdea0b009f8444cfdc9a13
dinesh@1947 <dinesh.stellardigital.in>
2025-05-07 16:10:04 +0530

    done

--- academy/api/endpoints/v1/__init__.py
+++ academy/api/endpoints/v1/__init__.py
@@ -6,4 +6,4 @@ from .import  user_courses
 router = APIRouter()
 
 router.include_router(users.router, prefix="/users", tags=["users"])
-# router.include_router(user_courses.router, prefix="/user-course", tags=["user-course"])
+router.include_router(user_courses.router, prefix="/user-course", tags=["user-course"])
