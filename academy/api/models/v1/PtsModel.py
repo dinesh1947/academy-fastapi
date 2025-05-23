7be4b37a985d22a30342be8ba8e43142d119410e
dinesh@1947 <dinesh.stellardigital.in>
2025-05-23 15:36:07 +0530

    done

--- academy/api/models/v1/PtsModel.py
+++ academy/api/models/v1/PtsModel.py
@@ -52,9 +52,8 @@ class TestSeries(Base):
     start_date_time: Mapped[Optional[datetime]] = mapped_column(DateTime, nullable=True)
     end_date_time: Mapped[Optional[datetime]] = mapped_column(DateTime, nullable=True)
     # image: Mapped[Optional[str]] = mapped_column(String(255), nullable=True)
-    # image: Mapped[Optional[str]] = mapped_column(Text, nullable=True)
-    image: Mapped[Optional[str]] = mapped_column(String(1024), nullable=True)
-
+    image: Mapped[Optional[str]] = mapped_column(Text, nullable=True)
+    
 
     url: Mapped[Optional[str]] = mapped_column(String(255), nullable=True)
     official_email: Mapped[Optional[str]] = mapped_column(String(255), nullable=True)
